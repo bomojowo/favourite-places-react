f7f69eff44b6f0fd0a8e1e33ab9a2c38ef9e2428
Bukola Omojowo <bomojowo@gmail.com>
2021-06-18 12:43:08 +0100

    Added functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import Place from './Place';
 import './App.css';
+import LondonEye from './images/london-eye.png'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="header-border">My Favorite Places </h1>
       </header>
+      <main className="main-content">
+        <Place
+          placeTitle={"London Eye"}
+          
+          placePicture={'placeholder for image'}
+          placeName={"London"}
+          placeCountry={"England"}
+          placeWhy={"Even though i dont like heights, its a nice why to bond with the person you are with. Also it's nice to see the whole of London whilst youre on it!"}
+          
+         />
+        <section>
+          <div>
+            <h3>Eveyrhting underneath this is just placeholder!</h3>
+          </div>
+         London Eye
+         <div>
+          <img className='london-eye' src={LondonEye} alt='London Eye' />
+         </div>
+        </section>
+        <section>
+         Effiel Tower
+        </section>
+        <section>
+         Leaning Tower of Pisa
+        </section>
+        <section>
+         Disney Land Florida
+        </section>
+      </main>
+      <footer className="footer-content">
+        <h1>Add Stuff Here</h1>
+      </footer>
     </div>
   );
 }
