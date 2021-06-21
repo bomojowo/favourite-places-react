e0fe77626c5a44570f514cd6b6e8c1f40a7c90b6
Bukola Omojowo <bomojowo@gmail.com>
2021-06-21 10:24:10 +0100

    completed first functional component

--- src/Place.tsx
+++ src/Place.tsx
@@ -1,32 +1,32 @@
-//import { Url } from "url";
-
 interface PlaceProps {
-    placeTitle: string;
-    placeName: string;
-    placeCountry: string;
-    placePicture: string;
-    placeWhy: string;
+  placeTitle: string;
+  placeName: string;
+  placeCountry: string;
+  placePicture: string;
+  placeWhy: string;
+  placeMap: string;
 }
 
 function Place(props: PlaceProps): JSX.Element {
-return(
+  return (
     <div>
-        <div className='cardHolder'>
-            <div className='placeTitle'>
-                {props.placeTitle}
-            </div>
-            <div className='placePicture'>
-                {props.placePicture}
-            </div>
-            <div className='placeCityCountry'>
-                {props.placeName}, {props.placeCountry}
-            </div>
-            <div className='placeWhy'>
-                {props.placeWhy}
-            </div>
+      <div className="cardHolder">
+        <div className="placeTitle">
+          <h3>{props.placeTitle}</h3>
         </div>
+        <img
+          className="placePicture"
+          src={props.placePicture}
+          alt="London Eye"
+        />
+        <div className="placeCityCountry">
+          {props.placeName}, {props.placeCountry}
+          <a href={props.placeMap}>(Map link)</a>
+        </div>
+        <div className="placeWhy">{props.placeWhy}</div>
+      </div>
     </div>
-)
+  );
 }
 
 export default Place;
