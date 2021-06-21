2eaf14245f3d85957a66aaea20c04af29d5a65b6
Bukola Omojowo <bomojowo@gmail.com>
2021-06-21 10:25:22 +0100

    Installed prettier and formated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,41 @@
-import React from 'react';
-import Place from './Place';
-import './App.css';
-import LondonEye from './images/london-eye.png'
-
+import React from "react";
+import Place from "./Place";
+import "./App.css";
+import LondonEye from "./images/london-eye.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="header-border">My Favorite Places </h1>
+        <h1 className="header-border">My Favourite Places </h1>
       </header>
       <main className="main-content">
         <Place
           placeTitle={"London Eye"}
-          
-          placePicture={'placeholder for image'}
+          placePicture={
+            "https://images.unsplash.com/photo-1523590564318-491748f70ea7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          }
           placeName={"London"}
           placeCountry={"England"}
-          placeWhy={"Even though i dont like heights, its a nice why to bond with the person you are with. Also it's nice to see the whole of London whilst youre on it!"}
-          
-         />
+          placeWhy={
+            "Even though i dont like heights, its good for bonding time. It's nice to see the whole of London at night too!"
+          }
+          placeMap={
+            "https://www.google.com/maps/d/viewer?mid=1Pj2ly5dkJoaHqvs6qMmwFA6uajQ&msa=0&ll=51.51532899201109%2C-0.20983750926209188&spn=0.00078%2C0.001682&z=18"
+          }
+        />
         <section>
           <div>
             <h3>Eveyrhting underneath this is just placeholder!</h3>
           </div>
-         London Eye
-         <div>
-          <img className='london-eye' src={LondonEye} alt='London Eye' />
-         </div>
+          London Eye
+          <div>
+            <img className="london-eye" src={LondonEye} alt="London Eye" />
+          </div>
         </section>
-        <section>
-         Effiel Tower
-        </section>
-        <section>
-         Leaning Tower of Pisa
-        </section>
-        <section>
-         Disney Land Florida
-        </section>
+        <section>Effiel Tower</section>
+        <section>Leaning Tower of Pisa</section>
+        <section>Disney Land Florida</section>
       </main>
       <footer className="footer-content">
         <h1>Add Stuff Here</h1>
