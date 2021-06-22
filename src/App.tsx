e5f2e65416e669e5b3938d63b1e8151a10871a8a
Bukola Omojowo <bomojowo@gmail.com>
2021-06-22 13:32:43 +0100

    extracted all data from json

--- src/App.tsx
+++ src/App.tsx
@@ -31,32 +31,20 @@ function App() {
            placeMap={Places.places[1].map}
         />
          <Place
-          placeTitle={"Leaning Tower of Pisa"}
-          placePicture={
-            "https://cdn.britannica.com/88/80588-050-8D944BFE/Leaning-Tower-of-Pisa-Italy.jpg"
-          }
-          placeName={"Pisa"}
-          placeCountry={"Italy"}
-          placeWhy={
-            "placeholder"
-          }
-          placeMap={
-            "https://www.google.com/maps/d/viewer?mid=1ckb0UEWs8I1USGNHmNJqnLBfFnc&ie=UTF8&oe=UTF8&msa=0&ll=43.723303999999985%2C10.394160999999999&z=17"
-          }
+           placeTitle={Places.places[2].title}
+           placePicture={Places.places[2].picture}
+           placeName={Places.places[2].name}
+           placeCountry={Places.places[2].country}
+           placeWhy={Places.places[2].why}
+           placeMap={Places.places[2].map}
         />
          <Place
-          placeTitle={"Disneyland Florida"}
-          placePicture={
-            "https://images.unsplash.com/photo-1599059721913-8df7f348af04?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzbmV5JTIwbGFuZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
-          }
-          placeName={"Orlando Florida"}
-          placeCountry={"Unied States"}
-          placeWhy={
-            "placeholder"
-          }
-          placeMap={
-            "https://www.google.com/maps/place/Walt+Disney+World%C2%AE+Resort/@28.3771857,-81.5729287,17z/data=!3m1!4b1!4m5!3m4!1s0x88dd7ee634caa5f7:0xa71e391fd01cf1a0!8m2!3d28.3771857!4d-81.57074"
-          }
+           placeTitle={Places.places[3].title}
+           placePicture={Places.places[3].picture}
+           placeName={Places.places[3].name}
+           placeCountry={Places.places[3].country}
+           placeWhy={Places.places[3].why}
+           placeMap={Places.places[3].map}
         />
       </main>
       <footer className="footer-content">
